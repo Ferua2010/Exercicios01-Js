8594de29d52ea463af6977d841c498785c360e3f
Ferua2010 <ruabragafelipe@gmail.com>
2026-06-19 08:49:25 -0300

    Exercicios05.js

--- Exercicios05.js
+++ Exercicios05.js
@@ -1,4 +1,6 @@
-//Recebe um numero e conta de 1 em 1 até chegar no numero recebido
+//Solicita um numero e conta de 1 em 1 até chegar no numero recebido
+//(Lembre-se de instalar o prompt-sync(npm install prompt-sync))
+
 prompt = require("prompt-sync")();
 
 let num1 = parseInt(prompt("Insira o numero inteiro ou positivo: "));
