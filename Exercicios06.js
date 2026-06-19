be379622c2531a2dd0a6e4ea38794506669d7658
Ferua2010 <ruabragafelipe@gmail.com>
2026-06-19 09:00:32 -0300

    Exercicios06.js

--- Exercicios06.js
+++ Exercicios06.js
@@ -1,4 +1,4 @@
-//Mostra uma regressiva de 1 em 1 e apartir de 30 e mostra os numeros na sequencia divisiveis por 4
+//Mostra uma regressiva de 1 em 1 apartir de 30 até 0 e mostra os numeros na sequencia divisiveis por 4:
 for (let i = 30; i >= 1; i--) {
   if (i % 4 === 0) {
     process.stdout.write(`[${i}] `);
